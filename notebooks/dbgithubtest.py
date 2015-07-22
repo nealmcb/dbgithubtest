2af58c892823e36985f8cb2e9569553a3468a69e
Neal McBurnett <neal@mcburnett.org>
2015-07-22 16:25:35 -0600

    test 6?

---
+++ notebooks/dbgithubtest.py
@@ -0,0 +1,22 @@
+# Databricks notebook source exported at Wed, 22 Jul 2015 22:25:35 UTC
+# MAGIC %md
+# MAGIC #Hello world!
+
+# COMMAND ----------
+
+x = 3
+print x
+
+# COMMAND ----------
+
+x * x
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC New cell
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC yet another new cell
